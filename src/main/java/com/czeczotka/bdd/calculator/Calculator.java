e66cdc1f06a3228b56861e5f9719f9905c525ecf
QMetryDev <38056220+QMetryDev@users.noreply.github.com>
2018-08-06 11:37:53 -0700

    Add files via upload

--- src/main/java/com/czeczotka/bdd/calculator/Calculator.java
+++ src/main/java/com/czeczotka/bdd/calculator/Calculator.java
@@ -7,7 +7,15 @@ public class Calculator {
     public void add(int arg1, int arg2) {
         result = arg1 + arg2;
     }
-
+    public void subtract(int arg1, int arg2) {
+        result = arg1 - arg2;
+    }
+    public void multiply(int arg1, int arg2) {
+        result = arg1 * arg2;
+    }
+    public void divide(int arg1, int arg2) {
+        result = arg1 / arg2;
+    }
     public int getResult() {
         return result;
     }
